4cea542a2cb1d3e692217c5341813b0c8a279d59
João Victor Mendes <mendesjoaovic@gmail.com>
2022-01-30 13:07:04 -0300

    Adicionando exercício sobre Pi

---
+++ exercicios/pi/pi-parallel.c
@@ -0,0 +1,112 @@
+/* 
+ ** Universidade Federal de São Carlos
+ ** Departamento de Computação
+ ** Prof. Hélio Crestana Guardia
+ ** Programação Paralela e Distribuída
+ */
+
+/*
+ ** Programa : 
+ ** Comando: 
+ ** Objetivo: observar passagem de valores para função da thread.
+              Nesse caso, usa-se vetor em que cada thread preenche 
+              os resultados da sua operação. 
+*/
+
+/* 
+ Algoritmo de Monte Carlo para cálculo do valor de pi 
+ Dados um círculo de raio 1 inserido em um quadrado de lado 2.
+ Relação entre pontos aleatórios que estão dentro do círculo 
+ e pontos no quadrado pode ser usada para cálculo de pi.
+
+ Relação entre as áreas: (pi*r^2)/(4*r^2)
+ Considerando r=1: (pi/4) = pontos_no_círculo/total_pontos
+ pi = 4 * pontos_no_círculo / total_pontos
+
+ Considerando a geração de valores aleatórios entre 0 e 1 (raio)
+ para as coordenadas x e y de um ponto, usa-se a equação do teorema 
+ de Pitágoras para determinar se ponto está dentro do círculo ou não:
+ z^2=x^2+y^2
+*/
+
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h> 
+#include <pthread.h> 
+#include <stdlib.h> 
+#include <limits.h> 
+
+#define MAX_THREADS 128 
+// #define SAMPLES 2147483647 
+// #define SAMPLES INT_MAX
+#define SAMPLES (1e6)
+
+int _num_threads = 1;
+int _samples = 0;
+
+void *calculate_hits(void *args) {
+	int samples = (_samples / _num_threads);
+	int hits = 0;
+
+	double x, y;
+	unsigned short xi[3];
+
+	for (int i=0; i< samples; i++) {
+		x=erand48(xi);
+		y=erand48(xi);
+		if(x*x+y*y <= 1.0)
+			hits++;
+	}
+
+	return (void *)hits;
+}
+
+int main(int argc, char **argv) 
+{ 
+	int i;
+	double x, y;
+	unsigned short xi[3];
+
+	pthread_t threads[MAX_THREADS]; 
+	int hits[MAX_THREADS];
+	int total_hits = 0;
+
+	if (argc>1) {
+		_num_threads = atoi(argv[1]);
+		if (_num_threads < 0 || _num_threads > MAX_THREADS) {
+			printf("Erro: max %d threads\n", MAX_THREADS);
+			return(EXIT_FAILURE);
+		}
+	} else {
+		_num_threads = MAX_THREADS;
+	}
+	if (argc>2)
+		_samples = atoi(argv[2]);
+	
+	if (_samples <= 0 || _samples > INT_MAX)
+		_samples = SAMPLES;
+
+	// Main computation
+	for (int j = 0; j < _num_threads; j++) {
+		int result_code = pthread_create(&threads[j], NULL, calculate_hits, NULL);
+    	assert(!result_code);
+	}
+
+	for (int j = 0; j < _num_threads; j++) {
+		void *val;
+		int result_code = pthread_join(threads[j], &val);
+		hits[j] = (int)val;
+    	assert(!result_code);
+	}
+
+	// Collecting results
+	for (int j = 0; j < _num_threads; j++) {
+		total_hits += (hits[j]);
+	}
+
+	printf("Pi: %.16f\n",(float)4 * (float)total_hits / (float)_samples);
+
+	return(0);
+} 
+
+
---
+++ exercicios/pi/results.md
@@ -0,0 +1,33 @@
+# Results
+Resultados em uma máquina com dois 2 cores físicos e 4 virtuais (HTT).
+
+### Sample = 10.000.000
+
+Program | Threads | Time | Computed Value |
+--------|---------|------|----------|
+pi.out| 1|0.24s|Pi: 3.1413516998291016|
+pi-parallel.out| 1|0.24s|Pi: 3.1415500640869141|
+pi-parallel.out| 4| 0.8s|Pi: 3.1423521041870117|
+pi-parallel.out| 8| 0.8s|Pi: 3.1423392295837402|
+pi-parallel.out|64| 0.8s|Pi: 3.1412992477416992|
+
+### Sample = 1.000.000.000
+
+Program | Threads | Time | Computed Value |
+--------|---------|------|----------|
+pi.out| 1|24.81s|Pi: 3.1415555477142334|
+pi-parallel.out| 1|23.98s|Pi: 3.1415491104125977|
+pi-parallel.out| 4| 8.11s|Pi: 3.1414895057678223|
+pi-parallel.out| 8| 8.08s|Pi: 3.1413512229919434|
+pi-parallel.out|64| 8.33s|Pi: 3.1416158676147461|
+
+
+### Sample = 10.000.000.000
+
+Program | Threads | Time | Computed Value |
+--------|---------|------|----------|
+pi.out| 1|33.95s|Pi: 3.1416199207305908|
+pi-parallel.out| 1|33.70s|Pi: 3.1415755748748779|
+pi-parallel.out| 4|11.57s|Pi: 3.1414873600006104|
+pi-parallel.out| 8|11.55s|Pi: 3.1414546966552734|
+pi-parallel.out|64|11.52s|Pi: 3.1415712833404541|
