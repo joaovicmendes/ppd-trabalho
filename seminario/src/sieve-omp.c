61346d3d7c1322982ef9268903b29e47776b5b95
João Victor Mendes <mendesjoaovic@gmail.com>
2022-04-16 20:27:23 -0300

    Versão com OpenMP

--- seminario/src/sieve-omp.c
+++ seminario/src/sieve-omp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 // #define DEBUG /* comment this line to avoid debbuging prints */
 
@@ -7,28 +8,50 @@ typedef unsigned int uint;
 #define IS_PRIME 1
 #define NOT_PRIME 0
 
+uint floor_sqrt(uint x)
+{
+    if (x == 0 || x == 1)
+    return x;
+ 
+    uint i = 1, result = 1;
+    while (result <= x)
+    {
+      i++;
+      result = i * i;
+    }
+    return i - 1;
+}
+
 void print(uint *primes, uint size)
 {
-    for (uint i=2; i < size; i++)
+    printf("2\n");
+    for (uint i=3; i < size; i += 2)
         if (primes[i])
             printf("%d\n", i);
 }
 
 uint sieve(uint n)
 {
+    uint sqrt_of_n = floor_sqrt(n);
     uint *primes = malloc( (n+1) * sizeof(uint) );
+    #pragma omp parallel for
     for (uint i=2; i < n+1; i++)
         primes[i] = IS_PRIME;
 
-    for (uint p=2; p*p <= n; p++)
+    // #pragma omp parallel for
+    // for (uint p=4; p <= n; p += 2)
+    //     primes[p] = NOT_PRIME;
+
+    #pragma omp parallel for schedule(dynamic)
+    for (uint p=3; p <= sqrt_of_n; p += 2)
         if (primes[p] == IS_PRIME)
             for (uint mult = p*p; mult < n+1; mult += p)
                 primes[mult] = NOT_PRIME;
 
-    uint count = 0;
-    for (uint i = 2; i < n+1; i++)
-        if (primes[i] == IS_PRIME)
-            count++;
+    uint count = 1;
+    #pragma omp parallel for reduction(+:count)
+    for (uint i = 3; i < n+1; i += 2)
+            count += primes[i];
     
 #ifdef DEBUG
     print(primes, n+1);
